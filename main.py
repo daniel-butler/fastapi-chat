c54f0db53c455129865596ff0487b0aac6ad5eac
Daniel Butler <dabutler89@gmail.com>
2019-09-14 17:15:30 -0400

    parameters

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-from enum import Enum
-
 from fastapi import FastAPI
-
-
-class ModelName(str, Enum):
-    alexnet = 'alexnet'
-    resnet = 'resnet'
-    lenet = 'lenet'
-
 
 app = FastAPI()
 
-
-@app.get("/model/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-    if model_name.value == 'lenet':
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-    return {"model_name": model_name, "message": "Have some residuals"}
+fake_db_items = [{"item_name": "truck"}, {"item_name": "airplane"}, {"item)_name": "dolphin"}]
 
 
+@app.get("/items/")
+async def read_item(skip:int = 0, limit: int = 10):
+    return fake_db_items[skip : skip + limit]
